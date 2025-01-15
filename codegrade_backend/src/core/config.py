691b13e98eaf5d0086acacfca5f6ffa2362202cc
Joseph Adebowale <adebowalejojo2001@gmail.com>
2025-01-15 19:09:49 +0100

    Add SQLITE DB to codegrade

--- codegrade_backend/src/core/config.py
+++ codegrade_backend/src/core/config.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 import warnings
 from typing import Annotated, Any, Literal
@@ -6,11 +7,9 @@ from pydantic import (
     AnyUrl,
     BeforeValidator,
     HttpUrl,
-    PostgresDsn,
     computed_field,
     model_validator,
 )
-from pydantic_core import MultiHostUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing_extensions import Self
 
@@ -55,18 +54,13 @@ class Settings(BaseSettings):
     POSTGRES_USER: str = ""
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
+    SQLITE_DATABASE_PATH: str
 
     @computed_field  # type: ignore[prop-decorator]
     @property
-    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
-        return MultiHostUrl.build(
-            scheme="postgresql+psycopg",
-            username=self.POSTGRES_USER,
-            password=self.POSTGRES_PASSWORD,
-            host=self.POSTGRES_SERVER,
-            port=self.POSTGRES_PORT,
-            path=self.POSTGRES_DB,
-        )
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        path = os.path.realpath(self.SQLITE_DATABASE_PATH)
+        return f"sqlite://{path}"
 
     SMTP_TLS: bool = True
     SMTP_SSL: bool = False
