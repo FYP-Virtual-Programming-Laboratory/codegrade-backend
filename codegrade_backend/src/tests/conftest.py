aab1964dee4cc80a921c020abfcfce79033c3e56
Joseph Adebowale <adebowalejojo2001@gmail.com>
2025-01-17 20:56:41 +0100

    Add CI to project

--- codegrade_backend/src/tests/conftest.py
+++ codegrade_backend/src/tests/conftest.py
@@ -2,11 +2,10 @@ from collections.abc import Generator
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, delete
+from sqlmodel import Session
 
 from src.core.db import engine, init_db
 from src.main import app
-from src.models import Item, User
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -14,11 +13,6 @@ def db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         init_db(session)
         yield session
-        statement = delete(Item)
-        session.execute(statement)
-        statement = delete(User)
-        session.execute(statement)
-        session.commit()
 
 
 @pytest.fixture(scope="module")
---
+++ codegrade_backend/src/tests/test_init.py
@@ -0,0 +1,3 @@
+def test_inti() -> None:
+    """Initial test will be removed later."""
+    assert True
